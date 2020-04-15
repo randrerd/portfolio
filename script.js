019ff640a4f5c421eebd78e55ad56eac00119e36
randrerd <raulromand@gmail.com>
2020-04-14 23:46:14 -0300

    style projects section with carousel

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ btn.onclick = function () {
   btn.classList.toggle("is-active");
   navLinks.classList.toggle("mobile");
 };
+
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  groupCells: true,
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+  // options
+});
