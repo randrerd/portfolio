85999e1ec63b4996657bfb762bb6bf9c8d75b519
randrerd <raulromand@gmail.com>
2020-07-31 21:30:50 -0300

    :recycle: Refactor JavaScript code for better readability

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
+"use strict";
+
+const navbarSection = (() => {
+  //DOM Cache
+  let btn = document.getElementById("navButton");
+  let navLinks = document.getElementById("navLinks");
+
+  //Event listeners
+  btn.onclick = function () {
+    btn.classList.toggle("is-active");
+    navLinks.classList.toggle("mobile");
+  };
+})();
+
+const carouselSection = (() => {
+  //DOM Cache
+  let elem = document.querySelector(".main-carousel");
+
+  var flkty = new Flickity(elem, {
+    // options
+    cellAlign: "left",
+    contain: true,
+    imagesLoaded: true,
+    groupCells: true,
+  });
+
+  var flkty = new Flickity(".main-carousel", {});
+})();
+
+//AOS Library Handler
+
 AOS.init({
   duration: 800,
-});
-
-
-let btn = document.getElementById("navButton");
-let navLinks = document.getElementById("navLinks");
-
-btn.onclick = function () {
-  btn.classList.toggle("is-active");
-  navLinks.classList.toggle("mobile");
-};
-
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true,
-  imagesLoaded: true,
-  groupCells: true,
-});
-
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
 });
